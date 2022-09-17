87f8262989f0b27c053a63fcbe17db9210db8aae
FURKAN GUNDUZ <fgunduz211@posta.pau.edu.tr>
2022-09-17 16:57:00 +0300

    Yeni Degisikler

--- MüsteriBilgileri.Designer.cs
+++ MüsteriBilgileri.Designer.cs
@@ -40,13 +40,37 @@
             this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtRoomNo = new System.Windows.Forms.TextBox();
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.mtbPhone = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.DateExitPicker = new System.Windows.Forms.DateTimePicker();
+            this.DateLoginPicker = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(43, 31);
+            this.button1.Location = new System.Drawing.Point(1048, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(237, 79);
+            this.button1.Size = new System.Drawing.Size(237, 64);
             this.button1.TabIndex = 0;
             this.button1.Text = "Show Me Informations";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,12 +90,13 @@
             this.columnHeader9,
             this.columnHeader10});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(17, 128);
+            this.listView1.Location = new System.Drawing.Point(12, 419);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1255, 543);
+            this.listView1.Size = new System.Drawing.Size(1273, 259);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
             // 
             // columnHeader1
             // 
@@ -122,19 +147,275 @@
             this.columnHeader10.Text = "Exit History";
             this.columnHeader10.Width = 137;
             // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(1048, 85);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(237, 59);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(1048, 271);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(237, 52);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(1048, 150);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(237, 54);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtRoomNo
+            // 
+            this.txtRoomNo.Enabled = false;
+            this.txtRoomNo.Location = new System.Drawing.Point(226, 321);
+            this.txtRoomNo.Name = "txtRoomNo";
+            this.txtRoomNo.Size = new System.Drawing.Size(298, 34);
+            this.txtRoomNo.TabIndex = 48;
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(226, 267);
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(298, 34);
+            this.txtTcNo.TabIndex = 47;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(226, 210);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(298, 34);
+            this.txtMail.TabIndex = 46;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(226, 86);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(298, 34);
+            this.txtSurname.TabIndex = 45;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(226, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(298, 34);
+            this.txtName.TabIndex = 44;
+            // 
+            // mtbPhone
+            // 
+            this.mtbPhone.Location = new System.Drawing.Point(226, 147);
+            this.mtbPhone.Mask = "(999) 000-0000";
+            this.mtbPhone.Name = "mtbPhone";
+            this.mtbPhone.Size = new System.Drawing.Size(298, 34);
+            this.mtbPhone.TabIndex = 43;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(33, 147);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 27);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "Customer Phone:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(32, 213);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(186, 27);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Customer E-Mail:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(23, 267);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 27);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Customer T.C No :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(104, 327);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 27);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Room No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(7, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 27);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Customer Surname:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(36, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 27);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Customer Name:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(701, 25);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(298, 34);
+            this.txtPrice.TabIndex = 54;
+            // 
+            // DateExitPicker
+            // 
+            this.DateExitPicker.Location = new System.Drawing.Point(701, 138);
+            this.DateExitPicker.Name = "DateExitPicker";
+            this.DateExitPicker.Size = new System.Drawing.Size(298, 34);
+            this.DateExitPicker.TabIndex = 53;
+            // 
+            // DateLoginPicker
+            // 
+            this.DateLoginPicker.Location = new System.Drawing.Point(701, 79);
+            this.DateLoginPicker.Name = "DateLoginPicker";
+            this.DateLoginPicker.Size = new System.Drawing.Size(298, 34);
+            this.DateLoginPicker.TabIndex = 52;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.ForeColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(560, 143);
+            this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 27);
+            this.label9.TabIndex = 51;
+            this.label9.Text = "Exit History:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.ForeColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(541, 85);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 27);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Login History:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(560, 31);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 27);
+            this.label7.TabIndex = 49;
+            this.label7.Text = "Room Price:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(987, 339);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 34);
+            this.textBox1.TabIndex = 55;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.ForeColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(815, 346);
+            this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(152, 27);
+            this.label10.TabIndex = 56;
+            this.label10.Text = "Search Name:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1048, 213);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(237, 52);
+            this.btnClear.TabIndex = 57;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
             // MüsteriBilgileri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1297, 690);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.DateExitPicker);
+            this.Controls.Add(this.DateLoginPicker);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtRoomNo);
+            this.Controls.Add(this.txtTcNo);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.mtbPhone);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "MüsteriBilgileri";
             this.Text = "MüsteriBilgileri";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -152,5 +433,29 @@
         private System.Windows.Forms.ColumnHeader columnHeader8;
         private System.Windows.Forms.ColumnHeader columnHeader9;
         private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtRoomNo;
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.MaskedTextBox mtbPhone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DateTimePicker DateExitPicker;
+        private System.Windows.Forms.DateTimePicker DateLoginPicker;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnClear;
     }
 }
--- MüsteriBilgileri.cs
+++ MüsteriBilgileri.cs
@@ -21,6 +21,8 @@ namespace PansionApplication
 
         private void Veriler()
         {
+            listView1.Items.Clear();
+            
             Connection.Open();
           SqlCommand command = new SqlCommand("select *from Tbl_AddCustomer",Connection);
 
@@ -29,6 +31,7 @@ namespace PansionApplication
             while (reader.Read())
             {
                 ListViewItem add=new ListViewItem();
+                
 
                 add.Text = reader["CustomerId"].ToString();
                 add.SubItems.Add(reader["CustomerName"].ToString());
@@ -50,6 +53,54 @@ namespace PansionApplication
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Veriler();
+        }
+
+        int id = 0;
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            id = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+
+            txtName.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            txtSurname.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            mtbPhone.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            txtMail.Text = listView1.SelectedItems[0].SubItems[4].Text;
+            txtTcNo.Text = listView1.SelectedItems[0].SubItems[5].Text;
+            txtRoomNo.Text = listView1.SelectedItems[0].SubItems[6].Text;
+            txtPrice.Text = listView1.SelectedItems[0].SubItems[7].Text;
+            DateLoginPicker.Text = listView1.SelectedItems[0].SubItems[8].Text;
+            DateExitPicker.Text = listView1.SelectedItems[0].SubItems[9].Text;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+            SqlCommand command = new SqlCommand("delete from Tbl_AddCustomer where" +
+                " CustomerId=(" + id + ")", Connection);
+            command.ExecuteNonQuery();
+            Connection.Close();
+            Veriler();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtSurname.Clear();
+            txtMail.Clear();
+            mtbPhone.Clear();
+            txtPrice.Clear();
+            txtRoomNo.Clear();
+            txtTcNo.Clear();
+            DateLoginPicker.Text = "";
+            DateExitPicker.Text = "";
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+            SqlCommand command = new SqlCommand("Update Tbl_AddCustomer set CustomerName='"+txtName.Text+"' , CustomerSurname='"+txtSurname.Text+"', CustomerPhone ='"+mtbPhone.Text+"', CustomerEmail ='"+txtMail.Text+"',CustomerRoomNo='"+txtRoomNo.Text+"',CustomerRoomPrice='"+txtPrice.Text+"',CustomerLoginHistory='"+DateLoginPicker.Value.ToString("yyyy-MM-dd")+"',CustomerExitHistory='"+DateExitPicker.Value.ToString("yyyy-MM-dd")+"' where CustomerId="+id+"",Connection);
+            command.ExecuteNonQuery();
+            Connection.Close();
             Veriler();
         }
     }
